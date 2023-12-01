13cac0ba3508272598d5f22c47728858b62122d5
Slyviaa <helloslyvia1996@163.com>
2023-12-01 17:48:57 +0800

    add

---
+++ blogs/leetCode/14二叉搜索树.js
@@ -0,0 +1,20 @@
+/* 
+  174. 寻找二叉搜索树中的目标节点
+
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} cnt
+ * @return {number}
+ */
+var findTargetNode = function(root, cnt) {
+
+};
