f7b2bfd06b4a965b8f5c0aa4bdba4d28a09f91c4
slyvia <1293730542@qq.com>
2023-12-27 08:35:32 +0800

    动态规划1

--- blogs/算法小册学习/18动态规划.js
+++ blogs/算法小册学习/18动态规划.js
@@ -112,36 +112,47 @@ var coinChange = function(coins, amount) {
     现在有一个容量为 c 的背包，问你如何选取物品放入背包，才能使得背包内的物品总价值最大？
 
   解题思路：
-  1.不是所有的物品都放在了背包里面；背包不一定恰好装满；
-  2.
+  1.不是所有的物品都放在了背包里面，背包不一定恰好装满；
+  2.逐渐增加背包容量时；遍历物品的容量，物品可能在背包内（影响总价值），可能不在背包内（不影响总价值）；
+  3.状态转移方程：
+    f(i，c)为总物品个数为i；背包容量为c时，能得到的最大价值；
+    f（i-1,c)为当拿掉所有物品中的一个物品，且此物品不在背包内的最大价值；
+    f（i-1,c-w[i])为当拿掉所有物品中的一个物品，且此物品不在背包内的最大价值；
+  f（i,c）= Math.max(f（i-1,c),f（i-1,c-w[i])+value[i]）
+  初始化状态:
+    当物品总数为0时，f(0，c) = 0 
+    当背包容量总数为0时，f(i，0) = 0
 */
 
 function testWeightBagProblem (weight, value, size) {
   // 定义 dp 数组
+  // 数组初始化
   const len = weight.length,
-        dp = Array(len).fill().map(() => Array(size + 1).fill(0));
+        dp = Array(len + 1).fill().map(() => Array(size + 1).fill(0));
 
-  // 初始化
-  for(let j = weight[0]; j <= size; j++) {
-      dp[0][j] = value[0];
-  }
-
-  // weight 数组的长度len 就是物品个数
-  for(let i = 1; i < len; i++) { // 遍历物品
-      for(let j = 0; j <= size; j++) { // 遍历背包容量
-          if(j < weight[i]) dp[i][j] = dp[i - 1][j];
-          else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+  // i 为物品数量；j为背包容量
+  for (let j = weight[0]; j <= size; j++) {
+    // 若背包有容量；初始化背包的
+    dp[1][j] = value[0];
+}
+// 遍历增加物品数量和背包容量
+  for(let i = 1 ; i <=len;i++ ){
+    for(let j =0 ; j <=size;j++ ){
+      if(j>=weight[i]){
+        // 如果当前背包的容量大于物品的重量；则该物品可放入在背包内
+        dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j - weight[i]] + value[i])
+      }else{
+        // 如果当前背包的容量小于物品的重量；则该物品可没有放入在背包内；最大价值和前一个最大价值一样
+        dp[i][j] = dp[i-1][j]
       }
+    }
   }
-
   console.table(dp)
-
-  return dp[len - 1][size];
+  return dp[len][size]
 }
 
 function test () {
   console.log(testWeightBagProblem([1, 3, 4, 5], [15, 20, 30, 55], 6));
 }
 
-// test();
-
+test();
